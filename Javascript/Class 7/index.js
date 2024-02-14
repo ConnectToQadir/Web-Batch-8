a2f6662ce4b735882b9120e08c5a7c93433cb343
Ghulam Qadir <connecttoqadir@gmail.com>
2024-02-14 18:52:29 +0500

    JS Class 7

---
+++ Javascript/Class 7/index.js
@@ -0,0 +1,97 @@
+// Array Methods
+
+// for(var i=0;i<ary.length;i++){
+//     console.log(ary[i]+2)
+// }
+// console.log(ary)
+
+
+// var output = ary.map(function (v,i){
+    //     return v+1
+    // })
+    
+    // console.log(output)
+    
+
+// var ary = [2,4,6]
+// ary.forEach(function (v,i){
+//     console.log(v+1)
+// })
+
+
+// var ary = [2,3,4,5,6,98,87,95]
+// var filtered = ary.filter(function (v,i){
+//     return v % 2 == 0
+// })
+
+// console.log(filtered)
+
+
+
+// Objects
+// var obj = {fName:"Ali",lName:"Raza",age:20}
+// obj.contact = "0315256526"
+// delete obj.age
+
+
+
+// Object.keys(obj).map(function (v,i){
+//     console.log(obj[v])
+// })
+
+
+// Array and Object Destructuring
+
+// var {fName,lName,age} = {fName:"Ali",lName:"Raza",age:20}
+// console.log(fName + " " + lName, age)
+
+// var [urdu,math,eng] = [60,98,65]
+// console.log(eng)
+
+
+// Objects
+// var name = "Ali"
+// var age = 23
+// var contact = "39832932932"
+
+// var obj = {name,age,contact}
+// console.log(obj)
+
+
+
+// Error Handling
+
+// try {
+    
+// } catch (error) {
+    
+// }finally{
+
+// }
+
+// console.log("Hello")
+// console.log("Hello")
+
+
+// Dependant on Server reponse
+// try {
+//     console.log(name)
+// } catch (error) {
+//     console.log(error)
+// }finally{
+//     console.log("Finally")
+// }
+
+// console.log("Hello")
+// console.log("Hello")
+// console.log("Hello")
+
+
+
+
+
+
+
+
+
+
