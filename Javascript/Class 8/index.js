64937f3b57ff697c1b51a5f318fe02316c481e75
Ghulam Qadir <connecttoqadir@gmail.com>
2024-02-17 19:06:05 +0500

    JS Class 8

---
+++ Javascript/Class 8/index.js
@@ -0,0 +1,37 @@
+//  Asynchronious & synchronious Bahaviour
+//  JS is synchronious language
+
+
+
+
+// line 1 -- Server Call -- 1min
+// line 2
+
+
+
+
+
+// fetch method is used to fetch data from a third party server
+
+
+
+
+async function fetchData(){
+    // var res = await (await fetch("https://jsonplaceholder.typicode.com/todos/1")).json()
+    // console.log(res)
+
+    // try {
+    //     var data = await fetch("https://jsonplaceholder.typicode.com/todo/1")
+    //     var jsonData = await data.json()
+    //     console.log(jsonData)
+    // } catch (error) {
+    //     console.log(error)
+    // }
+
+}
+
+fetchData()
+
+
+
+
